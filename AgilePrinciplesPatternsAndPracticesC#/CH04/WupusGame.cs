f1c77cef66b209a16044aa98e12c97ce7f890386
ZongLinYe <bluewingleaf@livemai.tw>
2023-10-21 17:26:14 +0800

    加入專案檔案。

---
+++ AgilePrinciplesPatternsAndPracticesC#/CH04/WupusGame.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgilePrinciplesPatternsAndPracticesC_.CH04
+{
+    public class WupusGame
+    {
+        public void Connect(int v1, int v2, string v3)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void East()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetPlayerRoom()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AgilePrinciplesPatternsAndPracticesC#Tests/CH04/WupusGameTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AgilePrinciplesPatternsAndPracticesC_.CH04;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgilePrinciplesPatternsAndPracticesC_.CH04.Tests
+{
+    [TestClass()]
+    public class WupusGameTests
+    {
+        [TestMethod()]
+        public void TestMove()
+        {
+            WupusGame game = new WupusGame();
+            game.Connect(1, 2, "N");
+            game.GetPlayerRoom();
+            game.East();
+            Assert.AreEqual(2, game.GetPlayerRoom());
+    
+        }      
+    }
+}
